fc1b467c321b6782f7f6b6b4e5325fb077375085
yuldakim <jeong8003@gmail.com>
2026-05-13 16:26:05 +0900

    1. 품질이 0이면 더 이상 감소하지 않음 테스트

--- cpp/test/GildedRoseTest.cpp
+++ cpp/test/GildedRoseTest.cpp
@@ -1,9 +1,19 @@
-#include <gtest/gtest.h>
 #include "GildedRose.h"
+#include <gtest/gtest.h>
 
 TEST(GildedRoseTest, Foo) {
-    std::vector<Item> items = { Item("foo", 0, 0) };
-    GildedRose app(items);
-    app.updateQuality();
-    EXPECT_EQ("fixme", app.items[0].name);
+  std::vector<Item> items = {Item("foo", 0, 0)};
+  GildedRose app(items);
+  app.updateQuality();
+  EXPECT_EQ("fixme", app.items[0].name);
+}
+
+// 1. 일반 아이템: 품질이 0이면 더 이상 감소하지 않음
+TEST(GildedRoseTest, NonameQualityZeroNoDecrease) {
+  std::vector<Item> items = {Item("noname", 0, 0)};
+  GildedRose app(items);
+  app.updateQuality();
+
+  EXPECT_EQ(-1, items[0].sellIn);
+  EXPECT_EQ(0, items[0].quality);
 }
