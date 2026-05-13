af66048d1f0332ebd4fab25f3d06ef3083de2b52
yuldakim <jeong8003@gmail.com>
2026-05-13 16:44:01 +0900

    test code 예시 주석처리 및 Sulfuras 유통기한 지났어도 변화없음

--- cpp/test/GildedRoseTest.cpp
+++ cpp/test/GildedRoseTest.cpp
@@ -1,12 +1,12 @@
 #include "GildedRose.h"
 #include <gtest/gtest.h>
 
-TEST(GildedRoseTest, Foo) {
-  std::vector<Item> items = {Item("foo", 0, 0)};
-  GildedRose app(items);
-  app.updateQuality();
-  EXPECT_EQ("fixme", app.items[0].name);
-}
+// TEST(GildedRoseTest, Foo) {
+//   std::vector<Item> items = {Item("foo", 0, 0)};
+//   GildedRose app(items);
+//   app.updateQuality();
+//   EXPECT_EQ("fixme", app.items[0].name);
+// }
 
 // 1. 일반 아이템: 품질이 0이면 더 이상 감소하지 않음
 TEST(GildedRoseTest, NonameQualityZeroNoDecrease) {
@@ -35,5 +35,15 @@ TEST(GildedRoseTest, SulfurasNoChangeAtSellInZero) {
   app.updateQuality();
 
   EXPECT_EQ(0, items[0].sellIn);
+  EXPECT_EQ(5, items[0].quality);
+}
+
+// 4. 전설템(Sulfuras): 유통기한 지났어도 변화 없음
+TEST(GildedRoseTest, SulfurasNoChangeEvenAfterSellIn) {
+  std::vector<Item> items = {Item("Sulfuras, Hand of Ragnaros", -1, 5)};
+  GildedRose app(items);
+  app.updateQuality();
+
+  EXPECT_EQ(-1, items[0].sellIn);
   EXPECT_EQ(5, items[0].quality);
 }
