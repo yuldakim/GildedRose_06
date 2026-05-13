7e6bb8143eaf1251307df6154555dd90b5c6d361
yuldakim <jeong8003@gmail.com>
2026-05-13 22:07:04 +0900

    Refactoring 3. Items[i] 보다는 참조문 사용하여 한눈에 들어오게 개선

--- cpp/src/GildedRose.cpp
+++ cpp/src/GildedRose.cpp
@@ -18,36 +18,39 @@ void decreaseQuality(Item &item) {
 
 void GildedRose::updateQuality() {
   for (size_t i = 0; i < items.size(); i++) {
+    // 3단계 Item[i] 보다는 참조문 사용하여 한눈에 들어오게 개선
+    Item &item = items[i];
+
     // 1단계 Sulfuras의 퀄리티와 유효기간은 변화가 없다.
     // 그래서 Loop 최상단에서 제외한다.
-    if (items[i].name == "Sulfuras, Hand of Ragnaros")
+    if (item.name == "Sulfuras, Hand of Ragnaros")
       continue;
 
-    if (items[i].name == "Aged Brie" ||
-        items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+    if (item.name == "Aged Brie" ||
+        item.name == "Backstage passes to a TAFKAL80ETC concert") {
       // 2단계 Aged Brie와 Backstage Passes가 10초과인경우
-      increaseQuality(items[i]);
-      if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (items[i].sellIn < 11)
-          increaseQuality(items[i]); // 2단계 Backstage passes Quality +2 증가
-        if (items[i].sellIn < 6)
-          increaseQuality(items[i]); // 2단계 Backstage passes Quality +3 증가
+      increaseQuality(item);
+      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.sellIn < 11)
+          increaseQuality(item); // 2단계 Backstage passes Quality +2 증가
+        if (item.sellIn < 6)
+          increaseQuality(item); // 2단계 Backstage passes Quality +3 증가
       }
     } else {
-      decreaseQuality(items[i]);
+      decreaseQuality(item);
     }
 
     // 2단계 유통기한 감소
-    items[i].sellIn = items[i].sellIn - 1;
+    item.sellIn = item.sellIn - 1;
 
     // 2단계 유통기간 만료 후 처리
-    if (items[i].sellIn < 0) {
-      if (items[i].name == "Aged Brie") {
-        increaseQuality(items[i]);
-      } else if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-        items[i].quality = 0;
+    if (item.sellIn < 0) {
+      if (item.name == "Aged Brie") {
+        increaseQuality(item);
+      } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+        item.quality = 0;
       } else {
-        decreaseQuality(items[i]);
+        decreaseQuality(item);
       }
     }
   }
